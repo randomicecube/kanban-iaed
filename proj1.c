74af638c4657b8e238de5628e52c9f8cca9cabbb
gaspa <diogo.marques.gaspar@gmail.com>
2021-03-26 21:43:55 +0000

    test

---
+++ proj1.c
@@ -0,0 +1,11 @@
+/*
+ * File: proj1.c
+ * Author: Diogo Gaspar, 99207 - LEIC-A
+ * Description: I honestly don't know yet.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
